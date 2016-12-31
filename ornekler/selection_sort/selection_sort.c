366c38f091c57a80b43ddfee7487f89484f9e010
Mücahit Demirhan <mucahitsec@gmail.com>
2016-12-31 03:35:09 +0200

    Update selection_sort.c

--- ornekler/selection_sort/selection_sort.c
+++ ornekler/selection_sort/selection_sort.c
@@ -1,12 +1,4 @@
-/*
- ============================================================================
- Name        : selection_sort.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
+
 
 #include <stdio.h>
 
