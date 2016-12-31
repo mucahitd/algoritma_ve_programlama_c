1689635e5364c78a1a93f1c9d8d2881a7dac6c54
Mücahit Demirhan <mucahitsec@gmail.com>
2016-12-31 03:15:32 +0200

    Update ilk_tek_son_cift.c

--- ornekler/ilk_tek_son_cift/ilk_tek_son_cift.c
+++ ornekler/ilk_tek_son_cift/ilk_tek_son_cift.c
@@ -1,12 +1,4 @@
-/*
- ============================================================================
- Name        : ilktek_son_cift.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
+
 
 #include <stdio.h>
 #include <stdlib.h>
