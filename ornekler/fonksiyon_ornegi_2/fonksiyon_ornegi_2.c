f90287bec9a608df2dabe0a78b4723278219c65c
Mücahit Demirhan <mucahitsec@gmail.com>
2016-12-31 03:06:12 +0200

    Update fonksiyon_ornegi_2.c

--- ornekler/fonksiyon_ornegi_2/fonksiyon_ornegi_2.c
+++ ornekler/fonksiyon_ornegi_2/fonksiyon_ornegi_2.c
@@ -1,12 +1,4 @@
-/*
- ============================================================================
- Name        : fonksiyon_ornegi_2.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
+
 
 
 #include <stdio.h>
