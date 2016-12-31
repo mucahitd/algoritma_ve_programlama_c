8d427cc21864cca70214d43ec4153cca2586dc34
Mücahit Demirhan <mucahitsec@gmail.com>
2016-12-31 02:41:54 +0200

    Update basamak_bulma_foksiyonu.c

--- ornekler/basamak_bulma_foksiyonu/basamak_bulma_foksiyonu.c
+++ ornekler/basamak_bulma_foksiyonu/basamak_bulma_foksiyonu.c
@@ -1,13 +1,3 @@
-/*
- ============================================================================
- Name        : basamak_bulma_foksiyonu.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
-
 
 
 #include <stdio.h>
