442847c45e878285eba1c41525f7b1aa1eb724ab
Mücahit Demirhan <mucahitsec@gmail.com>
2016-12-31 03:05:20 +0200

    Update fibonacci_dizisi.c

--- ornekler/fibonacci_dizisi/fibonacci_dizisi.c
+++ ornekler/fibonacci_dizisi/fibonacci_dizisi.c
@@ -1,12 +1,3 @@
-/*
- ============================================================================
- Name        : fibonacci_dizisi.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
 
 #include <stdio.h>
 
