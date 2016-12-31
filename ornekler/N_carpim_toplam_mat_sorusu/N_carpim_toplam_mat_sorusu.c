8622b86ea8f79387401bdbd54a7af050a9af3144
Mücahit Demirhan <mucahitsec@gmail.com>
2016-12-31 02:32:34 +0200

    Update N_carpim_toplam_mat_sorusu.c

--- ornekler/N_carpim_toplam_mat_sorusu/N_carpim_toplam_mat_sorusu.c
+++ ornekler/N_carpim_toplam_mat_sorusu/N_carpim_toplam_mat_sorusu.c
@@ -1,12 +1,3 @@
-/*
- ============================================================================
- Name        : N_carpim_toplam_mat_sorusu.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
 
 #include <stdio.h>
 
