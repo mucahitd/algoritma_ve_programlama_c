048405e563edd78a43825aad67a50da51566ba30
Mücahit Demirhan <mucahitsec@gmail.com>
2016-12-31 02:54:07 +0200

    Update dik_ucgen_tersi.c

--- ornekler/dik_ucgen_tersi/dik_ucgen_tersi.c
+++ ornekler/dik_ucgen_tersi/dik_ucgen_tersi.c
@@ -1,12 +1,4 @@
-/*
- ============================================================================
- Name        : dik_ucgen_tersi.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
+
 
 #include <stdio.h>
 
