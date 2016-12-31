9d770987a70cd34553e85b242d58b3ec069c0c39
Mücahit Demirhan <mucahitsec@gmail.com>
2016-12-31 02:50:24 +0200

    Update cumlede_karakter_sayisi.c

--- ornekler/cumlede_karakter_sayisi/cumlede_karakter_sayisi.c
+++ ornekler/cumlede_karakter_sayisi/cumlede_karakter_sayisi.c
@@ -1,12 +1,4 @@
-/*
- ============================================================================
- Name        : cumlede_karakter_sayisi.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
+
 
 #include<stdio.h>
 
