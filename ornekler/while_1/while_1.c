609994819f7753707321c3b9b3a823eb14dc89c7
Mücahit Demirhan <mucahitsec@gmail.com>
2016-12-31 03:41:35 +0200

    Update while_1.c

--- ornekler/while_1/while_1.c
+++ ornekler/while_1/while_1.c
@@ -1,12 +1,4 @@
-/*
- ============================================================================
- Name        : while_1.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
+
 
 #include <stdio.h>
 
