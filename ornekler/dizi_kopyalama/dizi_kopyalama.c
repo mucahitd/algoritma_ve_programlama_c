bd84f0c6c2daec0f23cccc2bb7e5d6016902c83b
Mücahit Demirhan <mucahitsec@gmail.com>
2016-12-31 02:54:45 +0200

    Update dizi_kopyalama.c

--- ornekler/dizi_kopyalama/dizi_kopyalama.c
+++ ornekler/dizi_kopyalama/dizi_kopyalama.c
@@ -1,12 +1,4 @@
-/*
- ============================================================================
- Name        : dizi_kopyalama.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
+
 
 #include <stdio.h>
 
