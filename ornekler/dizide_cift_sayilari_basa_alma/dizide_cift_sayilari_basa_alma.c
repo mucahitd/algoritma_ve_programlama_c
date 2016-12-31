c1470224f942e514fb6bdf892443724f3dc3f712
Mücahit Demirhan <mucahitsec@gmail.com>
2016-12-31 02:57:15 +0200

    Update dizide_cift_sayilari_basa_alma.c

--- ornekler/dizide_cift_sayilari_basa_alma/dizide_cift_sayilari_basa_alma.c
+++ ornekler/dizide_cift_sayilari_basa_alma/dizide_cift_sayilari_basa_alma.c
@@ -1,12 +1,4 @@
-/*
- ============================================================================
- Name        : dizide_cift_sayilari_basa_alma.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
+
 
 #include <stdio.h>
 
