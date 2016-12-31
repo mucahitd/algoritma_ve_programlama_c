a1190e59bfbd62b758e240baae6aa12b735b682f
Mücahit Demirhan <mucahitsec@gmail.com>
2016-12-31 03:36:08 +0200

    Update sifreleme_fonksiyonu.c

--- ornekler/sifreleme_fonksiyonu/sifreleme_fonksiyonu.c
+++ ornekler/sifreleme_fonksiyonu/sifreleme_fonksiyonu.c
@@ -1,12 +1,4 @@
-/*
- ============================================================================
- Name        : sifreleme_fonksiyonu.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
+
 
 #include<stdio.h>
 
