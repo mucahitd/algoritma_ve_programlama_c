73da7a58ca237d5e8b2db97e8013a399ff3c8f7c
Mücahit Demirhan <mucahitsec@gmail.com>
2016-12-31 03:29:55 +0200

    Update rastgele_sayi_uretmek.c

--- ornekler/rastgele_sayi_uretmek/rastgele_sayi_uretmek.c
+++ ornekler/rastgele_sayi_uretmek/rastgele_sayi_uretmek.c
@@ -1,12 +1,4 @@
-/*
- ============================================================================
- Name        : rastgele_sayi_uretmek.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
+
 
 
 #include<stdio.h>
