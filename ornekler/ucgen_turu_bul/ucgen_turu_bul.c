5bb11066152d2ecf15ae6dbd8fa5fbe6a90e12eb
Mücahit Demirhan <mucahitsec@gmail.com>
2016-12-31 03:40:28 +0200

    Update ucgen_turu_bul.c

--- ornekler/ucgen_turu_bul/ucgen_turu_bul.c
+++ ornekler/ucgen_turu_bul/ucgen_turu_bul.c
@@ -1,12 +1,3 @@
-/*
- ============================================================================
- Name        : ucgen_turu_bul.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
 
 #include <stdio.h>
 
