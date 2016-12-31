755ab914c8fc8102cc6f9cbde4eaac4c2a9085f7
Mücahit Demirhan <mucahitsec@gmail.com>
2016-12-31 02:37:07 +0200

    Update asal_sayi.c

--- ornekler/asal_sayi/asal_sayi.c
+++ ornekler/asal_sayi/asal_sayi.c
@@ -1,12 +1,4 @@
-/*
- ============================================================================
- Name        : asal_sayi.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
+
 #include <stdio.h>
 
 int main(){
