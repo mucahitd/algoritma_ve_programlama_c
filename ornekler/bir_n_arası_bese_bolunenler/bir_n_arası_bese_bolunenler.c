82fb98b42926636fd58a2858097ede5176ee4e06
Mücahit Demirhan <mucahitsec@gmail.com>
2016-12-31 02:43:08 +0200

    Update bir_n_arası_bese_bolunenler.c

--- ornekler/bir_n_arası_bese_bolunenler/bir_n_arası_bese_bolunenler.c
+++ ornekler/bir_n_arası_bese_bolunenler/bir_n_arası_bese_bolunenler.c
@@ -1,12 +1,4 @@
-/*
- ============================================================================
- Name        : bir_n_arası_bese_bolunenler.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
+
 
 #include <stdio.h>
 
