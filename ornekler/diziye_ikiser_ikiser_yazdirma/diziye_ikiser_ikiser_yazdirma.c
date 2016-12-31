395307b37ce440f3eb5fb7aa4195a0f6ef2f59fa
Mücahit Demirhan <mucahitsec@gmail.com>
2016-12-31 02:59:19 +0200

    Update diziye_ikiser_ikiser_yazdirma.c

--- ornekler/diziye_ikiser_ikiser_yazdirma/diziye_ikiser_ikiser_yazdirma.c
+++ ornekler/diziye_ikiser_ikiser_yazdirma/diziye_ikiser_ikiser_yazdirma.c
@@ -1,12 +1,4 @@
-/*
- ============================================================================
- Name        : diziye_ikiser_ikiser_yazdirma.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
+
 
 #include <stdio.h>
 
