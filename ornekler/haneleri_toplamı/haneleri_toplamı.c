0f8bac7bc46f2552b3615bd6f5169872a59e3acd
Mücahit Demirhan <mucahitsec@gmail.com>
2016-12-31 03:10:25 +0200

    Update haneleri_toplamı.c

--- ornekler/haneleri_toplamı/haneleri_toplamı.c
+++ ornekler/haneleri_toplamı/haneleri_toplamı.c
@@ -1,12 +1,4 @@
-/*
- ============================================================================
- Name        : haneleri_toplamı.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
+
 
 #include <stdio.h>
 
